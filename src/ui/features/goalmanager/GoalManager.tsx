e08aa43d3ec269ba7429e1e29538c9cda3bbed62
Suraj Negi <sn945626@gmail.com>
2026-09-05 18:16:49 +0530

    Persist selected goal emoji

--- src/ui/features/goalmanager/GoalManager.tsx
+++ src/ui/features/goalmanager/GoalManager.tsx
@@ -69,6 +69,7 @@ export function GoalManager(props: Props) {
     }
 
     dispatch(updateGoalRedux(updatedGoal))
+    updateGoalApi(props.goal.id, updatedGoal)
   }
 
   const updateNameOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
